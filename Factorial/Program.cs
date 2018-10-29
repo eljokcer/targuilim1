147f2372dab850bcb97dba907c83c3671b8f79c3
joaquin cuneo <joaquincuneo@hotmail.com>
2018-10-29 13:37:54 +0200

    fact

---
+++ Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1, i = 1;
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            while (i<=n)
+            {
+                sum *= i++; 
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
