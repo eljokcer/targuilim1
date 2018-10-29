3ce295b19bbfb823c6ec9d72168c5199eb56f69f
joaquin cuneo <joaquincuneo@hotmail.com>
2018-10-29 13:52:34 +0200

    Devide

---
+++ Devide 7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devide_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            while (i <= n)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine("Boom");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+
+        }
+    }
+}
